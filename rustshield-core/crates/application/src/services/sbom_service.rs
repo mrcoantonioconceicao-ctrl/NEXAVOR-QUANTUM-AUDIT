910468808e74230bc391b5eb3b95711fe4cfe530
Marco Antônio <mrcoantonioconceicao@gmail.com>
2026-09-20 21:46:32 -0300

    sec(oracle): critical patch applied to rustshield-core/crates/application/src/services/sbom_service.rs

--- rustshield-core/crates/application/src/services/sbom_service.rs
+++ rustshield-core/crates/application/src/services/sbom_service.rs
@@ -12,6 +12,6 @@ impl SbomService {
     #[must_use]
     pub fn generate_cyclonedx_json(component_name: &str, dependencies: &[Dependency]) -> String {
         let bom = Self::generate_cyclonedx(component_name, dependencies);
-        serde_json::to_string_pretty(&bom).unwrap_or_default()
+        serde_json::to_string_pretty(&bom)
     }
 }
